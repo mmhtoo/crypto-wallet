aa1667e5c533628bb6bf39eea321e109a0347c75
mmhtoo <mmhtoo.dev@gmail.com>
2024-07-03 11:53:06 +0630

    feat: sample bottom tab with three screens

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,6 @@ export {default as BackArrow} from './back-arrow.svg';
 export {default as GetStartIcon} from './GetStartIcon';
 export {default as CrossIcon} from './CrossIcon';
 export {default as LockIcon} from './LockIcon';
+export {default as WalletIcon} from './wallet.svg';
+export {default as HistoryIcon} from './history.svg';
+export {default as ProfileIcon} from './profile.svg';
--- src/navigators/RootBottomTabNavigator.tsx
+++ src/navigators/RootBottomTabNavigator.tsx
@@ -1,6 +1,13 @@
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import {
+  BottomTabBarProps,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
+import {darkTheme} from '../constants';
+import React, {FC} from 'react';
+import {Pressable, StyleSheet, TextStyle, View} from 'react-native';
+import {Text} from 'react-native-paper';
+import {SvgProps} from 'react-native-svg';
+import {HistoryIcon, ProfileIcon, WalletIcon} from 'assets/icons';
 import {RootBottomTabScreenList} from 'types/react-navigation/declarations';
 
 const Tab = createBottomTabNavigator<RootBottomTabScreenList>();
@@ -11,6 +18,7 @@ export default function RootBottomTabNavigator() {
       screenOptions={{
         headerShown: false,
       }}
+      tabBar={CustomTabBar}
       sceneContainerStyle={styles.sceneContainer}>
       <Tab.Screen
         name={'Wallet'}
@@ -28,8 +36,80 @@ export default function RootBottomTabNavigator() {
   );
 }
 
+type TBottomTabItem = {
+  label: string;
+  Icon: FC<SvgProps>;
+  href: keyof RootBottomTabScreenList;
+};
+
+const TAB_BAR_SCREENS: Array<TBottomTabItem> = [
+  {
+    label: 'Wallet',
+    Icon: WalletIcon,
+    href: 'Wallet',
+  },
+  {
+    label: 'History',
+    Icon: HistoryIcon,
+    href: 'TransactionHistory',
+  },
+  {
+    label: 'Profile',
+    Icon: ProfileIcon,
+    href: 'Profile',
+  },
+];
+
+const CustomTabBar: FC<BottomTabBarProps> = ({state, navigation}) => {
+  const containerStyle = StyleSheet.compose(styles.tabBarContainer, {
+    backgroundColor: darkTheme.colors?.background,
+  });
+  const activeIndex = state.index;
+  const labelStyle = (isActive: boolean): TextStyle => ({
+    color: isActive ? darkTheme.colors?.primary : 'white',
+  });
+
+  return (
+    <View style={containerStyle}>
+      {TAB_BAR_SCREENS.map((item, index) => {
+        const isActive = index === activeIndex;
+        return (
+          <Pressable
+            key={item.href}
+            onPress={() => navigation.navigate(item.href)}>
+            <View style={styles.tabBarItemContainer}>
+              <item.Icon
+                width={20}
+                height={20}
+                fill={isActive ? darkTheme.colors?.primary : 'white'}
+              />
+              <Text style={labelStyle(index === activeIndex)}>
+                {item.label}
+              </Text>
+            </View>
+          </Pressable>
+        );
+      })}
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   sceneContainer: {
-    backgroundColor: '#000',
+    backgroundColor: '#010104',
+  },
+  tabBarContainer: {
+    width: '100%',
+    paddingHorizontal: 32,
+    flexDirection: 'row',
+    columnGap: 32,
+    justifyContent: 'space-between',
+  },
+  tabBarItemContainer: {
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    alignItems: 'center',
+    rowGap: 8,
   },
 });
